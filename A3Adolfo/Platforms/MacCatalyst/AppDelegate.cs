9c22d9a8872e7228eecca92247fa4d6b96b487d9
Adolfo David Romero <Romeroad@Sheridancollege.ca>
2025-03-28 18:29:50 -0400

    Added empty content pages and tested app on iOS emulator.

--- A3Adolfo/Platforms/MacCatalyst/AppDelegate.cs
+++ A3Adolfo/Platforms/MacCatalyst/AppDelegate.cs
@@ -5,5 +5,5 @@ namespace A3Adolfo;
 [Register("AppDelegate")]
 public class AppDelegate : MauiUIApplicationDelegate
 {
-	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
 }
---
+++ A3Adolfo/ViewRequestsPage.xaml.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A3Adolfo;
+
+public partial class ViewRequestsPage : ContentPage
+{
+    public ViewRequestsPage()
+    {
+        InitializeComponent();
+    }
+}
