a87071174d0d3dd1f834517f2427eab85b51120e
Adolfo David Romero <Romeroad@Sheridancollege.ca>
2025-04-01 19:43:38 -0400

    Added SVG assests and loosely populated classes

--- A3Adolfo/BusinessLogic/MeetingRoom.cs
+++ A3Adolfo/BusinessLogic/MeetingRoom.cs
@@ -2,5 +2,21 @@ namespace A3Adolfo.BusinessLogic;
 
 public class MeetingRoom
 {
+    //fields
+    private string _roomNumber; //Required
+    private int _seatingCapacity;
+    private RoomLayoutType _roomLayoutType;
+    private string _roomImageFileName_; //Required
+    private string _roomTypeIcon; //Temp
     
+    //constructor
+    public MeetingRoom(string roomNumber, int seatingCapacity, RoomLayoutType roomLayoutType, string roomImageFileName, string roomTypeIcon)
+    {
+        this._roomNumber = roomNumber;
+        this._seatingCapacity = seatingCapacity;
+        this._roomLayoutType = roomLayoutType;
+        this._roomImageFileName_ = roomImageFileName;
+        this._roomTypeIcon = roomTypeIcon;
+    }
+
 }
--- A3Adolfo/BusinessLogic/ReservationRequest.cs
+++ A3Adolfo/BusinessLogic/ReservationRequest.cs
@@ -1,6 +1,29 @@
+using Java.Sql;
+
 namespace A3Adolfo.BusinessLogic;
 
 public class ReservationRequest
 {
+    //fields
+    private int _requestId;
+    private string _requestBy;
+    private string _descripton;
+    private string _meetingDate;
+    private string _startTime;
+    private string _endTime;
+    private int _partcipantCount;
+    private RequestStatus _requestStatus;
     
+    //constructor
+    public ReservationRequest(int requestId, string requestBy, string descripton, string meetingDate, string startTime, string endTime, int partcipantCount, RequestStatus requestStatus)
+    {
+        this._requestId = requestId;
+        this._requestBy = requestBy;
+        this._descripton = descripton;
+        this._meetingDate = meetingDate;
+        this._startTime = startTime;
+        this._endTime = endTime;
+        this._partcipantCount = partcipantCount;
+        this._requestStatus = requestStatus;
+    }
 }
