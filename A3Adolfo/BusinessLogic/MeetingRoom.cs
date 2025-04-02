a136c3778a88958cdd4a0c3b2c9a790d57132d4c
Adolfo David Romero <Romeroad@Sheridancollege.ca>
2025-04-01 20:21:44 -0400

    Made MeetingRoom.cs more robust

--- A3Adolfo/BusinessLogic/MeetingRoom.cs
+++ A3Adolfo/BusinessLogic/MeetingRoom.cs
@@ -6,17 +6,56 @@ public class MeetingRoom
     private string _roomNumber; //Required
     private int _seatingCapacity;
     private RoomLayoutType _roomLayoutType;
-    private string _roomImageFileName_; //Required
-    private string _roomTypeIcon; //Temp
+    private string _roomImageFileName; //Required
+    private string _roomTypeIcon;
     
     //constructor
-    public MeetingRoom(string roomNumber, int seatingCapacity, RoomLayoutType roomLayoutType, string roomImageFileName, string roomTypeIcon)
+    /// <summary> Represents the possible meeting venues. </summary>
+    public MeetingRoom(string roomNumber, string roomImageFileName)
     {
+        //Required
         this._roomNumber = roomNumber;
-        this._seatingCapacity = seatingCapacity;
-        this._roomLayoutType = roomLayoutType;
-        this._roomImageFileName_ = roomImageFileName;
-        this._roomTypeIcon = roomTypeIcon;
+        this._roomImageFileName = roomImageFileName;
+        //defualts
+        this.SeatingCapacity = 1; 
+        this.RoomLayoutType = RoomLayoutType.Classroom; 
     }
-
+    
+    //Properties
+    public string RoomNumber
+    {
+        get => _roomNumber;
+        set
+        {
+            if (string.IsNullOrWhiteSpace(value))
+                throw new ArgumentException("Required: Room number.");
+            _roomNumber = value;
+        }
+    }
+    public int SeatingCapacity
+    {
+        get => _seatingCapacity;
+        set
+        {
+            if (value <= 0)
+                throw new ArgumentException("Seating capacity must be greater than 0.");
+            _seatingCapacity = value;
+        }
+    }
+    public RoomLayoutType RoomLayoutType
+    {
+        get => _roomLayoutType;
+        set => _roomLayoutType = value;
+    }
+    public string RoomImageFileName
+    {
+        get => _roomImageFileName;
+        set
+        {
+            if (string.IsNullOrWhiteSpace(value))
+                throw new ArgumentException("Required: Room image file name.");
+            _roomImageFileName = value;
+        }
+    }
+    public string RoomTypeIcon => $"{RoomLayoutType}.svg"; //Computed Property
 }
